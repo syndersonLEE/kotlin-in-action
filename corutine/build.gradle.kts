fb41a96c56c594bcf6ce8a9cde3adbf7fe125355
syndersonLEE <winnery93@gmail.com>
2022-09-11 11:25:11 +0900

    update corutine project

---
+++ corutine/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation(kotlin("stdlib"))
+}
